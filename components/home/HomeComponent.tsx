d15e557740da3075d6312ce8ec25a723e3d17cbf
MdodoIng <mdodoyt2@gmail.com>
2023-01-02 10:05:59 +0530

    modified:   components/feathersCategories/inOneLine/productsInOneLine.tsx 	modified:   components/home/HomeComponent.tsx

--- components/home/HomeComponent.tsx
+++ components/home/HomeComponent.tsx
@@ -34,10 +34,10 @@ const HomeComponent = (): any => {
               style={{ width: '100%', height: '100%' }}
             >
               <div className="relative z-auto grid h-full content-center overflow-hidden rounded-3xl p-10 md:pl-20">
-                <Image width={1440} height={1000} loading='lazy'
+                <Image width={1440} height={1000} loading='lazy' blurDataURL={item.picture}
                   className="absolute top-0 left-0 -z-10 h-full w-full object-cover"
-                  // src={item.picture}
-                  src={'https://cdn.shopify.com/s/files/1/0652/4570/8532/files/slider-1-2.jpg?v=1659427625&width=2100'}
+                  src={item.picture}
+                
                   alt="wallpaper"
                 />
                 <motion.h1
