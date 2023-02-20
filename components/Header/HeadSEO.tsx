023e02f4404d33e2635cf529879c6f8734e28b96
dodo <110053481+MdodoIng@users.noreply.github.com>
2023-02-20 09:13:08 +0000

    modified:   components/Header/HeadSEO.tsx

--- components/Header/HeadSEO.tsx
+++ components/Header/HeadSEO.tsx
@@ -7,11 +7,10 @@ const HeadSEO = ({ site = {}, page = {}, schema }: any) => {
     <Head>
 
       <meta charSet="utf-8" />
-      <meta name="description" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="format-detection" content="telephone=no" />
-
+      <meta name="description" content="Discover our top-quality exports and unparalleled customer service. As a trusted exporting company, we pride ourselves on delivering excellence in every shipment. Browse our extensive range of products today and experience the difference." />
       <link rel="icon" href="redbg.svg" sizes="any" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
